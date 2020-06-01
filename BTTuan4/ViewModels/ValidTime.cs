957971322f4ee250492287b62ad53735a9fa4c7b
Administrator <Administrator@PC209>
2020-06-01 16:21:50 +0700

    Lab4 index.html

---
+++ BTTuan4/ViewModels/ValidTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BTTuan4.ViewModels
+{
+    public class ValidTime : ValidationAttribute
+    {
+        // GET: FutureDate
+        public override bool IsValid(object value)
+        {
+            DateTime datetime;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out datetime);
+            return (isValid && datetime > DateTime.Now);
+        }
+    }
+}
